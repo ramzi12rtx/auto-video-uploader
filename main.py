ab9f703270b1e42bd1aee9687a47b3c3a353d44a
ramzi12rtx <ughggvbg@gmail.com>
2025-07-22 20:21:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from uploader import upload_video
 generate_video("هذا الفيديو تم إنشاؤه تلقائيًا 😎")
 upload_video()
 from google_auth_oauthlib.flow import InstalledAppFlow
+import json
+client_config = json.loads(os.environ["CLIENT_SECRET_JSON"])
+flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
